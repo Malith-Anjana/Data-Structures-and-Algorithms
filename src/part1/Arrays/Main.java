02081dc9b8d2ffc8c06532f2b789748ea837f8cb
Malith Anjana <malithanjana97@gmail.com>
2024-07-06 10:24:27 +0530

    create LinkList from scratch

--- src/part1/Arrays/Main.java
+++ src/part1/Arrays/Main.java
@@ -29,9 +29,9 @@ public class Main {
         list.add(4);
         list.add(7);
         list.add(5);
-        list.add(8);
-        list.add(3);
-        list.remove(3);
+        list.addFirst(8);
+        list.addLast(7);
+        list.remove(2);
         System.out.println(list.contains(5));
 
         Object [] regArray = list.toArray();
