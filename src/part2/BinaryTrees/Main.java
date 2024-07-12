93234a4c2fb34a0205c7902ef1b0ed5560d12a5a
Malith Anjana <malithanjana97@gmail.com>
2024-07-12 14:34:05 +0530

    Implement Binary Search Tree algorithms

--- src/part2/BinaryTrees/Main.java
+++ src/part2/BinaryTrees/Main.java
@@ -2,23 +2,48 @@ package part2.BinaryTrees;
 
 public class Main {
     public static void main(String[] args){
-        //Tree
-        //Node(value, leftChild, rightChild)
-        // Inert(value)
-        // find(value) return boolean
-
         //Create Binary Tree - Interview Question
         Tree tree = new Tree();
         tree.insert(7);
+        tree.insert(4);
         tree.insert(9);
-        tree.insert(5);
+        tree.insert(1);
         tree.insert(6);
-        tree.insert(3);
         tree.insert(8);
+        tree.insert(10);
 
-        System.out.println(tree.find(11));
-        System.out.println("done");
+        Tree tree2 = new Tree();
+        tree2.insert(7);
+        tree2.insert(4);
+        tree2.insert(9);
+        tree2.insert(1);
+        tree2.insert(6);
+        tree2.insert(8);
+        tree2.insert(10);
 
+        var find = tree.find(10);
+        System.out.println(find);
+
+        tree.traversePreOrder();
+        tree.traverseInOrder();
+        tree.traversePostOrder();
+
+        System.out.println((tree.height()));
+        System.out.println(tree.min() );
+
+        //Checking equality of 2 trees using recursion
+        System.out.println(tree.equals(tree2));
+
+        //check whether is Binary search tree
+        System.out.println(tree.isBinarySearchTree());
+
+        //Nodes at K distance from the route
+        var items = tree.getNodesAtDistance(0);
+        for (int item : items)
+            System.out.println(item);
+
+        //Level order traversal
+        tree.traversalLevelOrder();
 
         //Recursion - ex: Calculate factorial function
         System.out.println(factorial(5));
