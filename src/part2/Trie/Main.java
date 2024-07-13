4bf36ae7b1e9d4c04ab0b2c57ae719f016a45c25
Malith Anjana <malithanjana97@gmail.com>
2024-07-13 19:41:24 +0530

    Implement Trie algorithms

---
+++ src/part2/Trie/Main.java
@@ -0,0 +1,16 @@
+package part2.Trie;
+
+public class Main {
+    public static void main(String[] args){
+        //run using debugger
+        var trie = new Trie();
+        trie.insert("car");
+        trie.insert("card");
+        trie.insert("care");
+        trie.insert("careful");
+        trie.insert("egg");
+        trie.traverse();
+        System.out.println(trie.contains("car"));
+        System.out.println(trie.findWords("car"));
+    }
+}
