af44172b2977a5b2d96d78d49b58bd27164dc789
Malith Anjana <malithanjana97@gmail.com>
2024-07-17 13:50:25 +0530

    Implement jump search algorithm

--- src/Algorithms/Searching_Algorithms/TernarySearch/Main.java
+++ src/Algorithms/Searching_Algorithms/TernarySearch/Main.java
@@ -4,8 +4,8 @@ package Algorithms.Searching_Algorithms.TernarySearch;
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {2, 6, 9, 10, 13, 17, 25, 33};
-        var sorter = new TernarySearch();
-        var index = sorter.ternarySearch(numbers, 6);
+        var searcher = new TernarySearch();
+        var index = searcher.ternarySearch(numbers, 6);
 
         System.out.println(index);
     }
